06e371fec126da6032a8acd9fd605638113c6823
Víðir Valberg Guðmundsson <valberg@orn.li>
2023-03-25 20:33:25 +0100

    Add a management command to list all urls.

---
+++ django_view_decorator/management/commands/list_views.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+
+from django_view_decorator.apps import ViewRegistry
+
+
+
+class Command(BaseCommand):
+    help = "List all views in the project."
+
+    def handle(self, *args, **options):
+        for namespace, view in ViewRegistry.views.items():
+            for name, views in view.items():
+                for _view in views:
+                    meta_data = f"name: {name}, view: {_view.view.__module__}.{_view.view.__name__}"
+                    if isinstance(_view.paths, str):
+                        self.stdout.write(f"{_view.paths} ({meta_data})")
+                    else:
+                        for path in _view.paths:
+                            self.stdout.write(f"{path} ({meta_data})")
